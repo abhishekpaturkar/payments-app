65f62e9aca74abda8ff77b43c4de5587fbfaf902
abhishekpaturkar <godkillop03@gmail.com>
2024-07-04 14:26:53 +0530

    added check for already processed onramp txn

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,55 +1,69 @@
-import express from "express";
-import db from "@repo/db/client";
-const app = express();
+import express from "express"
+import db from "@repo/db/client"
+const app = express()
 
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
-    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    const paymentInformation: {
-        token: string;
-        userId: string;
-        amount: string
-    } = {
-        token: req.body.token,
-        userId: req.body.user_identifier,
-        amount: req.body.amount
-    };
+  //TODO: Add zod validation here?
+  //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+  //TODO: Check transaction is already successful then return already processed (Done)
+  const paymentInformation: {
+    token: string
+    userId: string
+    amount: string
+  } = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  }
 
-    try {
-        await db.$transaction([
-            db.balance.updateMany({
-                where: {
-                    userId: Number(paymentInformation.userId)
-                },
-                data: {
-                    amount: {
-                        // You can also get this from your DB
-                        increment: Number(paymentInformation.amount)
-                    }
-                }
-            }),
-            db.onRampTransaction.updateMany({
-                where: {
-                    token: paymentInformation.token
-                }, 
-                data: {
-                    status: "Success",
-                }
-            })
-        ]);
+  const alreadyProcessedTxn = await db.onRampTransaction.findFirst({
+    where: {
+      token: paymentInformation.token,
+      status: "Success",
+    },
+  })
 
-        res.json({
-            message: "Captured"
-        })
-    } catch(e) {
-        console.error(e);
-        res.status(411).json({
-            message: "Error while processing webhook"
-        })
-    }
+  if (alreadyProcessedTxn) {
+    // console.log(alreadyProcessedTxn)
+    return res.json({
+      message: "Already processed",
+    })
+  }
 
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            // You can also get this from your DB
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ])
+
+    res.json({
+      message: "Captured",
+    })
+  } catch (e) {
+    console.error(e)
+    res.status(411).json({
+      message: "Error while processing webhook",
+    })
+  }
 })
 
-app.listen(3003);
+app.listen(3003)
--- apps/user-app/components/OnRampTransactions.tsx
+++ apps/user-app/components/OnRampTransactions.tsx
@@ -22,7 +22,10 @@ export const OnRampTransactions = ({
     <Card title="Recent Transactions">
       <div className="">
         {transactions.map((t) => (
-          <div className="flex justify-between border-b border-slate-300 pb-2">
+          <div
+            className="flex justify-between border-b border-slate-300 pb-2"
+            key={t.time.toString()}
+          >
             <div>
               <div className="text-sm">Received INR</div>
               <div className="text-slate-600 text-xs">
